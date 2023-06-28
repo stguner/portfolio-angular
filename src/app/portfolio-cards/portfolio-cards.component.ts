82b37a4360ad6e36a561b01013ea3c5f15075e6d
stguner <gunersuleymanturker@gmail.com>
2023-06-28 17:47:21 +0300

    cards denemesi

---
+++ src/app/portfolio-cards/portfolio-cards.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-cards',
+  templateUrl: './portfolio-cards.component.html',
+  styleUrls: ['./portfolio-cards.component.css']
+})
+export class PortfolioCardsComponent {
+  isim = "Süleyman Türker";
+  soyisim = "GÜNER";
+}
